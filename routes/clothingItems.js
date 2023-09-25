c1f49829f681a31a47de897dd49a31392f237380
kaylieryan <kayliemryan@gmail.com>
2023-09-25 07:17:27 -0700

    build router.

---
+++ routes/clothingItems.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+const { createItem } = require('../controllers/clothingItems');
+
+//CRUD
+
+//CREATE
+router.post('/', createItem);
+
+module.exports = router;
