c372ec0062c65d98790de4a2a03acacbb2ec7b0e
kaylieryan <kayliemryan@gmail.com>
2023-09-25 06:55:22 -0700

    fix mongoos issue

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
+mongoose
+  .connect("mongodb://localhost:27017/wtwr_db")
+  .then((r) => {
+    console.log("connected to DB");
+  })
+  .catch((e) => {
+    (e) => console.log("DB error", e);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
   console.log("This is working");
 });
+
