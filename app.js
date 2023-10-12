eb186b5840da1484460295696729c995dbfa21d3
kaylieryan <kayliemryan@gmail.com>
2023-10-12 07:31:06 -0700

    Delete stuff.

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-
-// const { PORT = 3001 } = process.env;
-// const { handleNonExistentRoute } = require("./utils/errors");
-
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/wtwr_db")
-//   .then((r) => {
-//     console.log("connected to DB");
-//   })
-//   .catch((e) => {
-//     (e) => console.log("DB error", e);
-//   });
-
-// const app = express();
-// app.use(express.json());
-
-// app.use(handleNonExistentRoute);
-
-// const routes = require("./routes");
-
-// app.use(routes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}.`);
-// });
-
 const express = require("express");
 
 const mongoose = require("mongoose");
