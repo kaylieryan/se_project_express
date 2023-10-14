2db8601e600b6227d20bc68afd723cb91243a283
kaylieryan <kayliemryan@gmail.com>
2023-10-14 12:52:09 -0700

    implement helmet

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const helmet = require("helmet");
-
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const limiter = require("./models/middlewares/rateLimiter");
+
+const app = express();
 
 const { PORT = 3001 } = process.env;
 const { handleNonExistentRoute } = require("./utils/errors");
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
-const app = express();
 app.use(helmet());
 app.use(express.json());
 
@@ -23,6 +24,8 @@ app.use("/users", require("./routes/user"));
 app.use("/items", require("./routes/clothingItem"));
 
 app.use(handleNonExistentRoute);
+
+app.use(limiter);
 
 app.listen(PORT, () => {
   console.log("Port is running");
