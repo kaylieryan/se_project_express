29e3b6d81984b9f72d7070b0129ab36505d1dc03
kaylieryan <kayliemryan@gmail.com>
2023-11-01 12:07:18 -0700

    clean up code.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -21,28 +21,3 @@ module.exports = (req, res, next) => {
 
   return next();
 };
-
-// const jwt = require("jsonwebtoken");
-
-// const { JWT_SECRET } = require("../utils/config");
-// const { UNAUTHORIZED } = require("../utils/errors");
-
-// const auth = (req, res, next) => {
-//   const { authorization } = req.headers;
-
-//   if (!authorization || !authorization.startsWith("Bearer ")) {
-//     return res.status(UNAUTHORIZED).json({ message: "Unauthorized" });
-//   }
-
-//   const token = authorization.replace("Bearer ", "");
-
-//   try {
-//     const payload = jwt.verify(token, JWT_SECRET);
-//     req.user = payload;
-//     next();
-//   } catch (err) {
-//     return res.status(UNAUTHORIZED).json({ message: "Unauthorized" });
-//   }
-// };
-
-// module.exports = auth;
