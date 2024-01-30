1e96297527e62cc885499da3998300c5e8077fdf
kaylieryan <kayliemryan@gmail.com>
2024-01-30 14:19:12 -0700

    working through api issues.

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-//const userAuth = require("../middlewares/auth");
 
 const {
   getClothingItems,
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
-const { getCurrentUser, editCurrentUser } = require("../controllers/user");
+const {
+  getCurrentUser,
+  editCurrentUser,
+  createUser,
+} = require("../controllers/user");
 const userAuth = require("../middlewares/auth");
 
 router.get("/me", userAuth, getCurrentUser);
 
 router.patch("/me", userAuth, editCurrentUser);
+
+router.post("/", userAuth, createUser);
 
 module.exports = router;
