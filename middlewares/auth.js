0a4a279215b1eb0cbb9740fdba3a5be163518db8
kaylieryan <kayliemryan@gmail.com>
2024-02-20 17:09:47 -0700

    errors.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,13 @@
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../utils/config");
-const { UnauthorizedError } = require("../utils/UnauthorizedError");
-
-module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith("Bearer ")) {
-    next(new UnauthorizedError("authorization required"));
-  }
-
-  const token = authorization.replace("Bearer ", "");
-  let payload;
-
-  try {
-    payload = jwt.verify(token, JWT_SECRET);
-  } catch (err) {
-    next(new UnauthorizedError("authorization required"));
-  }
-  req.user = payload;
-  return next();
-};
+// const jwt = require("jsonwebtoken");
+// const { JWT_SECRET } = require("../utils/config");
+// const { AUTHORIZATION_ERROR } = require("../utils/errors");
 
 // module.exports = (req, res, next) => {
 //   const { authorization } = req.headers;
 //   if (!authorization || !authorization.startsWith("Bearer ")) {
-//     return res.status(authError).send({ message: "authorization required" });
+//     return res
+//       .status(AUTHORIZATION_ERROR)
+//       .send({ message: "authorization required" });
 //   }
 
 //   const token = authorization.replace("Bearer ", "");
@@ -31,10 +15,33 @@ module.exports = (req, res, next) => {
 //   try {
 //     payload = jwt.verify(token, JWT_SECRET);
 //   } catch (err) {
-//     return res.status(authError).send({ message: "authorization required" });
+//     return res
+//       .status(AUTHORIZATION_ERROR)
+//       .send({ message: "authorization required" });
 //   }
 
 //   req.user = payload;
 
 //   return next();
 // };
+
+const jwt = require("jsonwebtoken");
+const { authError } = require("../utils/errors");
+const { JWT_SECRET } = require("../utils/config");
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(authError).send({ message: "authorization required" });
+  }
+  const token = authorization.replace("Bearer ", "");
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res.status(authError).send({ message: "authorization required" });
+  }
+
+  req.user = payload;
+  return next();
+};
