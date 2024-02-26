56733596096df23a8be936f5d26c225ac3ba02c0
kaylieryan <kayliemryan@gmail.com>
2024-02-26 10:34:41 -0700

    add logging statements

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -30,15 +30,23 @@ const { authError } = require("../utils/errors");
 const { JWT_SECRET } = require("../utils/config");
 
 module.exports = (req, res, next) => {
+  console.log("auth middleware");
+  // log the endpoint being called
+  console.log(req.originalUrl);
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith("Bearer ")) {
+    console.log("auth error due to no auth");
     return res.status(authError).send({ message: "authorization required" });
   }
   const token = authorization.replace("Bearer ", "");
   let payload;
   try {
+    console.log("verifying token");
     payload = jwt.verify(token, JWT_SECRET);
+    console.log("verified token");
   } catch (err) {
+    console.log("auth error due to invalid token");
+    // console.log(err);
     return res.status(authError).send({ message: "authorization required" });
   }
 
