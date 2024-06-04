b00746c6c1cb340c65b04c4390772045b825c416
kaylieryan <kayliemryan@gmail.com>
2024-06-04 09:36:13 -0700

    updage config file.

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,3 @@
-// const JWT_SECRET = "SecretPassword123";
-
-// module.exports = { JWT_SECRET };
-
 const { JWT_SECRET = "SecretPassword123" } = process.env;
 
-module.exports = { JWT_SECRET, };
+module.exports = { JWT_SECRET };
